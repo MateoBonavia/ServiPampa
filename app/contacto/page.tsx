c33dd38394a4a4ae96b819cd6594e2f0893b3802
Mateo <mateobonavia@gmail.com>
2025-04-08 15:39:40 -0300

    ready contact page mobile

--- app/contacto/page.tsx
+++ app/contacto/page.tsx
@@ -1,4 +1,11 @@
 import Maps from '@/components/maps';
+import Image from 'next/image';
+import phone from '@/assets/svg/Contact/phone.svg';
+import whatsapp from '@/assets/svg/Contact/whatsapp.svg';
+import facebook from '@/assets/svg/Contact/facebook.svg';
+import instagram from '@/assets/svg/Contact/instagram.svg';
+import location from '@/assets/svg/Contact/location-dot.svg';
+import clock from '@/assets/svg/Contact/clock.svg';
 
 export default function ContactoPage() {
   const locationMorrison = {
@@ -12,15 +19,15 @@ export default function ContactoPage() {
   };
 
   return (
-    <div className="flex flex-col gap-2">
+    <div className="flex flex-col gap-8">
       <div className="mt-8">
         <Maps />
       </div>
 
       {/* Contact form */}
-      <div>
-        <div>
-          <h2 className="text-2xl font-bold text-center">Contacto</h2>
+      <div className="bg-[#ACACC4] rounded-lg p-2">
+        <div className="flex flex-col gap-2">
+          <h2 className="text-2xl font-semibold">Contacto</h2>
           <p>
             Completa tus datos y dejanos tu mensaje. Te respondemos lo antes
             posible!
@@ -30,31 +37,111 @@ export default function ContactoPage() {
           <input
             type="text"
             placeholder="Nombre"
-            className="border border-gray-300 p-2 rounded"
+            className="border border-[#000] p-2 rounded"
           />
           <input
             type="email"
             placeholder="Email"
-            className="border border-gray-300 p-2 rounded"
+            className="border border-[#000] p-2 rounded"
           />
           <textarea
             placeholder="Mensaje"
-            className="border border-gray-300 p-2 rounded h-32"
+            className="border border-[#000] p-2 rounded h-32"
           ></textarea>
-          <button className="bg-blue-500 text-white p-2 rounded">Enviar</button>
+          <div className="flex justify-end">
+            <button className="bg-[#00001C] text-white p-2 rounded w-30">
+              Enviar
+            </button>
+          </div>
         </form>
       </div>
 
       {/* Contact data */}
-      <div>
-        <div>
-          <h3>Morrison</h3>
-          <div></div>
+      <div className="bg-[#ACACC4] rounded-lg p-2 flex flex-col gap-10 mb-4">
+        {/* Morrison and Escalante data */}
+        <div className="flex justify-between">
+          <div className="flex flex-col gap-4">
+            <h3 className="text-xl font-semibold">Morrison</h3>
+            <div className="flex flex-col gap-4">
+              {/* Phone */}
+              <div className="flex gap-2 items-center">
+                <Image src={phone} alt="teléfono" className="w-6" />
+                <p>3537592323</p>
+              </div>
+              {/* WhatsApp */}
+              <div className="flex gap-2 items-center">
+                <Image src={whatsapp} alt="WhatsApp" className="w-6" />
+                <p>3537592323</p>
+              </div>
+              {/* Facebook */}
+              <div className="flex gap-2 items-center">
+                <Image src={facebook} alt="Facebook" className="w-6" />
+                <p>Servipampa</p>
+              </div>
+              {/* Instagram */}
+              <div className="flex gap-2 items-center">
+                <Image src={instagram} alt="Instagram" className="w-6" />
+                <p>Servipampa</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-4">
+            <h3 className="text-xl font-semibold">Escalante</h3>
+            <div className="flex flex-col gap-4">
+              {/* Phone */}
+              <div className="flex gap-2 items-center">
+                <Image src={phone} alt="teléfono" className="w-6" />
+                <p>3533439336</p>
+              </div>
+              {/* WhatsApp */}
+              <div className="flex gap-2 items-center">
+                <Image src={whatsapp} alt="WhatsApp" className="w-6" />
+                <p>3533439336</p>
+              </div>
+              {/* Facebook */}
+              <div className="flex gap-2 items-center">
+                <Image src={facebook} alt="Facebook" className="w-6" />
+                <p>Servipampa</p>
+              </div>
+              {/* Instagram */}
+              <div className="flex gap-2 items-center">
+                <Image src={instagram} alt="Instagram" className="w-6" />
+                <p>Servipampa</p>
+              </div>
+            </div>
+          </div>
         </div>
 
-        <div>
-          <h3>Escalante</h3>
-          <div></div>
+        {/* Location and time */}
+        <div className="flex flex-col gap-6">
+          {/* Morrison */}
+          <div className="flex flex-col gap-2">
+            {/* Location */}
+            <div className="flex items-center gap-2">
+              <Image src={location} alt="Ubicación" className="w-6" />
+              <p>Ruta Nac. 9 KM 515 - Morrison, Cordoba</p>
+            </div>
+            {/* Time */}
+            <div className="flex items-center gap-2">
+              <Image src={clock} alt="Horario" className="w-6" />
+              <p>Horarios</p>
+            </div>
+          </div>
+
+          {/* Escalante */}
+          <div className="flex flex-col gap-2">
+            {/* Location */}
+            <div className="flex items-center gap-2">
+              <Image src={location} alt="Ubicación" className="w-6" />
+              <p>Ruta Prov. 3 Y 11 - W. Escalante, Cordoba</p>
+            </div>
+            {/* Time */}
+            <div className="flex items-center gap-2">
+              <Image src={clock} alt="Horario" className="w-6" />
+              <p>Horarios</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
